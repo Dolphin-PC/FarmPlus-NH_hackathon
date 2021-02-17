69b39802dfd8de568cdc24a589a4827b49534a48
Chanyoung Park <43159931+Dolphin-PC@users.noreply.github.com>
2021-02-17 14:46:53 +0900

    Delete src/app directory

--- src/app/info.js
+++
@@ -1,8 +0,0 @@
-// export const serverUrl = "http://49.50.173.12:4000";
-export const serverUrl = "http://192.168.0.102:5000";
-
-export const Iscd = "000452";
-export const FintechApsno = "001";
-export const ApiSvcCd = "DrawingTransferA";
-export const AccessToken =
-   "35c93aa0acf5ab79ecdf806705a07531d237e78aaf666316b718ff652dee66fd";
