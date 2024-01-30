9c9cbcd12faa9b1f2c00a1fdb31572f9830b8fea
dolphin-pc <pcx474@gmail.com>
2024-01-31 01:10:01 +0900

    [modify] 새로고침해도, 유저정보 저장

--- src/components/dialogs/NoticeDialog.jsx
+++ src/components/dialogs/NoticeDialog.jsx
@@ -7,58 +7,51 @@ import NoticeCardComp from "../cards/NoticeCardComp";
 import ColumnCardComp from "../cards/ColumnCardComp";
 
 const NoticeDialog = (props) => {
-   const { onClose, open } = props;
+  const { onClose, open } = props;
 
-   const handleClose = () => {
-      onClose();
-   };
+  const handleClose = () => {
+    onClose();
+  };
 
-   const dispatch = useDispatch();
-   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
-   const handleOnRefresh = () => {
-      dispatch(getUserInfo(user));
-   };
+  const handleOnRefresh = () => {
+    dispatch(getUserInfo(user));
+  };
 
-   const DialogRender = (props) => {
-      return (
-         <Dialog fullScreen open={open} onClose={handleClose}>
-            <DialogTitle>
-               수신된 알림 &ensp;
-               <Refresh onClick={handleOnRefresh} className="RefreshIcon" />
-               <Cancel
-                  style={{ position: "fixed", top: 20, right: 20 }}
-                  onClick={handleClose}
-               />
-            </DialogTitle>
+  const DialogRender = (props) => {
+    return (
+      <Dialog fullScreen open={open} onClose={handleClose}>
+        <DialogTitle>
+          수신된 알림 &ensp;
+          <Refresh onClick={handleOnRefresh} className="RefreshIcon" />
+          <Cancel style={{ position: "fixed", top: 20, right: 20 }} onClick={handleClose} />
+        </DialogTitle>
 
-            <DialogContent>
-               <ColumnCardComp />
-               {props.children}
-            </DialogContent>
-         </Dialog>
-      );
-   };
+        <DialogContent>
+          <ColumnCardComp />
+          {props.children}
+        </DialogContent>
+      </Dialog>
+    );
+  };
 
-   if (
-      user.user.notice === "" ||
-      user.user.notice === undefined ||
-      user.user.notice === null
-   ) {
-      return (
-         <DialogRender>
-            <p>수신된 알림이 없습니다.</p>
-         </DialogRender>
-      );
-   }
-
-   return (
+  if (user.user.notice === "" || user.user.notice === undefined || user.user.notice === null) {
+    return (
       <DialogRender>
-         {user.user.notice.map((noti, index) => (
-            <NoticeCardComp key={index} {...noti} />
-         ))}
+        <p>수신된 알림이 없습니다.</p>
       </DialogRender>
-   );
+    );
+  }
+
+  return (
+    <DialogRender>
+      {user.user.notice.map((noti, index) => (
+        <NoticeCardComp key={index} {...noti} />
+      ))}
+    </DialogRender>
+  );
 };
 
 export default NoticeDialog;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,45 +1,40 @@
 // import {} from '../actions/types'
 
-import {
-   ADD_FAVORITE,
-   CLEAR_USER,
-   SET_FAVORITE,
-   SET_USER,
-   GET_USER,
-} from "../actions/types";
+import { ADD_FAVORITE, CLEAR_USER, SET_FAVORITE, SET_USER, GET_USER } from "../actions/types";
 
 const initialState = {
-   user: null,
+  user: JSON.parse(localStorage.getItem("user")),
 };
 
 export default function userReducer(state = initialState, action) {
-   switch (action.type) {
-      case SET_USER:
-         return {
-            ...state,
-            user: action.payload,
-         };
-      case GET_USER:
-         return {
-            ...state,
-            user: action.payload,
-         };
-      case CLEAR_USER:
-         return {
-            ...state,
-            user: null,
-         };
-      case ADD_FAVORITE:
-         return {
-            ...state,
-            [state.user.favorite]: [...state.user.favorite, action.payload],
-         };
-      case SET_FAVORITE:
-         return {
-            ...state,
-            [state.user.favorite]: action.payload,
-         };
-      default:
-         return state;
-   }
+  switch (action.type) {
+    case SET_USER:
+      localStorage.setItem("user", JSON.stringify(action.payload));
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case CLEAR_USER:
+      return {
+        ...state,
+        user: null,
+      };
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        [state.user.favorite]: [...state.user.favorite, action.payload],
+      };
+    case SET_FAVORITE:
+      return {
+        ...state,
+        [state.user.favorite]: action.payload,
+      };
+    default:
+      return state;
+  }
 }
